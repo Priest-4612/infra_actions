f3379ea4e26274d99fbc1fbfddeed0dc1e4d1c9d
Priest4612 <priest4612@gmail.com>
2021-12-18 04:00:22 +0300

    Поправил ответ страниц

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -5,13 +5,13 @@ from django.http import HttpResponse
 
 def index(request):
     res = HttpResponse()
-    res.content = 'У меня получилось!'
     res.status_code = HTTPStatus.OK
+    res.content = 'У меня получилось!'
     return res
 
 
 def second_page(request):
     res = HttpResponse()
-    res.content = 'А это вторая страница'
     res.status_code = HTTPStatus.OK
+    res.content = 'А это вторая страница!'
     return res
